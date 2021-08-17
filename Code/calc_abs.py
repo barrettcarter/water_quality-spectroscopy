3cb2033078bd16228a50f6b6f4cede7167d255c4
J. Barrett Carter <barrettcarter1991@gmail.com>
2021-08-17 18:49:54 -0400

    modified code - added baseline correction

--- Code/calc_abs.py
+++ Code/calc_abs.py
@@ -7,21 +7,20 @@ import os
 print('...done loading modules')
 #%%
 ################################################# FUNCTIONS
-def calc_abs(sampleFile,referenceFile,specDirectory):
+def calc_abs(sample_array,reference_array,baseline_corr = False, 
+             wavelengths = 'missing'):
 
-    global refSpec_array  
-    global sampleSpec_array
+    if type(wavelengths) == str:
+        wavelengths = np.linspace(1,len(sample_array))
 
-    sampleName = sampleFile
-    refName = referenceFile
-
-    sampleSpec_df = pd.read_csv(specDirectory+sampleName)
-    refSpec_df = pd.read_csv(specDirectory+refName)
-
-    sampleSpec_array = pd.DataFrame.to_numpy(sampleSpec_df.INT)
-    refSpec_array = pd.DataFrame.to_numpy(refSpec_df.INT)
-   
-    absorbance = np.log10(refSpec_array/sampleSpec_array)
+    if baseline_corr == False:
+        absorbance = np.log10(reference_array/sample_array)
+        
+    else:
+        
+        sample_array = sample_array - min(sample_array)
+        reference_array = reference_array - min(reference_array)
+        absorbance = np.log10(reference_array/sample_array)
 
     return absorbance
 #%%
@@ -35,32 +34,53 @@ refName = 'spec_ref_0deg_22500.0_UFABE_2021-01-08T10%3A15%3A35.160053.csv'      
 refSpec_df = pd.read_csv(specDir+refName)
 waveLengths = pd.DataFrame.to_numpy(refSpec_df.WVL)
 
+ref = refSpec_df.INT
+ref = ref.to_numpy()
+
 sample1Name = 'spec_hat_unfiltered_01-05-2021_0deg_22500.0_UFABE_2021-01-08T10%3A16%3A08.936184.csv'
 sample2Name = 'spec_posnw16_unfiltered_01-05-2021_0deg_22500.0_UFABE_2021-01-08T10%3A30%3A12.475933.csv'
 
 sample1 = pd.read_csv(specDir+sample1Name)
-
+sample1 = sample1.INT
+sample1 = sample1.to_numpy()
 sample2 = pd.read_csv(specDir+sample2Name)
+sample2 = sample2.INT
+sample2 = sample2.to_numpy()
+
+
 #%%
 ### Calculate absorbances
 
-spec1 = calc_abs(sample1Name,refName,specDir)
-spec2 = calc_abs(sample2Name,refName,specDir)
+abs_1 = calc_abs(sample1,ref,wavelengths = waveLengths)
+abs_2 = calc_abs(sample2,ref,wavelengths = waveLengths)
+
+abs_1_corr = calc_abs(sample1,ref,wavelengths = waveLengths,baseline_corr=True)
+abs_2_corr = calc_abs(sample2,ref,wavelengths = waveLengths,baseline_corr=True)
 #%%
 
 ### make plots
 plt.figure()
-plt.plot(waveLengths,spec1,label = 'hat unfiltered')
-plt.plot(waveLengths,spec2,label = 'posnw16 unfiltered')
-plt.title('otow lysimeter samples')
+plt.plot(waveLengths[0:300],abs_1[0:300],label = 'hat unfiltered')
+plt.plot(waveLengths[0:300],abs_2[0:300],label = 'posnw16 unfiltered')
+plt.title('original (uncorrected)')
+plt.xlabel('wavelength (nm)')
+plt.ylabel('absorbance')
+plt.legend(loc='upper right')
+
+plt.figure()
+plt.plot(waveLengths[0:300],abs_1_corr[0:300],label = 'hat unfiltered')
+plt.plot(waveLengths[0:300],abs_2_corr[0:300],label = 'posnw16 unfiltered')
+plt.ylim(0,1.75)
+plt.title('baseline corrected')
 plt.xlabel('wavelength (nm)')
 plt.ylabel('absorbance')
 plt.legend(loc='upper right')
 
 
+
 plt.figure()
-plt.plot(waveLengths,sample1.INT,label = 'hat unfiltered')
-plt.plot(waveLengths,sample2.INT,label = 'posnw16 unfiltered')
+plt.plot(waveLengths,sample1,label = 'hat unfiltered')
+plt.plot(waveLengths,sample2,label = 'posnw16 unfiltered')
 plt.plot(waveLengths,refSpec_df.INT,label = 'reference')
 plt.title('spectra')
 plt.xlabel('wavelength (nm)')
