85b121380625d12475549411eaa3293c66582ba9
J. Barrett Carter <barrettcarter1991@gmail.com>
2021-09-03 16:11:05 -0400

    updated HNSr code and data
    This version saves outputs in a dataframe and creates a good graph of results.

--- Hydroponics/code/abs-wq_an_HNSr.py
+++ Hydroponics/code/abs-wq_an_HNSr.py
@@ -22,9 +22,9 @@ from sklearn.utils import resample
 from sklearn.metrics import mean_squared_error as MSE
 from sklearn.ensemble import RandomForestRegressor
 
-# for looking up available scorers
-# import sklearn.metrics
-# sorted(sklearn.metrics.SCORERS.keys())
+#for looking up available scorers
+import sklearn.metrics
+sorted(sklearn.metrics.SCORERS.keys())
 
 #%%
 
@@ -113,81 +113,88 @@ for wq_row in range(wq_df.shape[0]):
 ## Nitrate
 ## PlSR
 
-param_grid = [{'n_components':np.arange(1,20)}]
+for rs in range(10):
 
-# keep = abs_wq_df['Name'].isin(['hogdn','hat'])
-# X = abs_wq_df.loc[:,'band_1':'band_1024'].to_numpy()
-X = abs_wq_df.loc[:,'band_1':'band_1024']
-Y = abs_wq_df['Nitrate-N']
-# name_dum = pd.get_dummies(abs_wq_df['Name'])
-# filtered_dum = pd.get_dummies(abs_wq_df['Filtered'])
-# X = abs_wq_df.loc[keep,:]
-# Y = abs_wq_df.Nitrate[keep].to_numpy()
-# name_dum = pd.get_dummies(abs_wq_df['Name'][keep])
-# X = pd.concat([name_dum,X],axis=1).to_numpy()
-# X = name_dum
-
-
-X_train, X_test, y_train, y_test = train_test_split(X, Y, random_state=2,
-                                                    test_size = 0.3)
-# test_names = X_test.Name.reset_index(drop=True)
-# test_filt = X_test.Filtered.reset_index(drop=True)
-
-# X_train = X_train.loc[:,'band_1':'band_1024']
-# X_test = X_test.loc[:,'band_1':'band_1024']
-pls = PLSRegression()
-clf = GridSearchCV(pls,param_grid,scoring = 'neg_mean_absolute_error')
-# clf.cv_results_
-# clf = GridSearchCV(pls,param_grid)
-clf.fit(X_train,y_train)
-n_comp = clf.best_params_['n_components']
-print('Number of components:\t'+str(n_comp))
-pls_opt = clf.best_estimator_
-Y_hat = pls_opt.predict(X_test)
-Y_hat_train = pls_opt.predict(X_train)
-
-r_sq = pls_opt.score(X_test,y_test)
-print('Test r-squared value:\t'+str(round(r_sq,3)))
-
-r_sq_train = pls_opt.score(X_train,y_train)
-print('Training r-squared value:\t'+str(round(r_sq_train,3)))
-
-
-#%%
-### Plot results
-# plt.plot(Y_hat,y_test,'b.')
-
-line11 = np.linspace(min(np.concatenate((y_test,Y_hat[:,0]))),
-                     max(np.concatenate((y_test,Y_hat[:,0]))))
-
-# lr = LinearRegression().fit(Y_hat,y_test)
-# linelr = lr.predict(line11.reshape(-1,1))
-
-plt.plot(y_test,Y_hat,'o',markersize = 4, label = 'predictions')
-plt.plot(line11,line11,label= '1:1 line')
-# plt.plot(line11,linelr,label = 'regression line')
-plt.xlabel('Lab Measured Nitrate (mg/L)')
-plt.ylabel('Predicted Nitrate (mg/L)')
-plt.text(170,200,r'$r^2 =$'+str(np.round(r_sq,3)))
-plt.title('Test Set')
-plt.legend()
-plt.show()
-
-line11 = np.linspace(min(np.concatenate((y_train,Y_hat_train[:,0]))),
-                     max(np.concatenate((y_train,Y_hat_train[:,0]))))
-
-# lr = LinearRegression().fit(Y_hat,y_test)
-# linelr = lr.predict(line11.reshape(-1,1))
-
-plt.plot(y_train,Y_hat_train,'o',markersize = 4, label = 'predictions')
-plt.plot(line11,line11,label= '1:1 line')
-# plt.plot(line11,linelr,label = 'regression line')
-plt.title('Training Set')
-plt.xlabel('Lab Measured Nitrate (mg/L)')
-plt.ylabel('Predicted Nitrate (mg/L)')
-plt.text(0.8*max(line11),min(line11),r'$r^2 =$'+str(np.round(r_sq_train,3)))
-plt.legend()
-plt.show()
+    param_grid = [{'n_components':np.arange(1,20)}]
+    
+    # keep = abs_wq_df['Name'].isin(['hogdn','hat'])
+    # X = abs_wq_df.loc[:,'band_1':'band_1024'].to_numpy()
+    Y = abs_wq_df['Nitrate-N']
+    keep = Y>0
+    X = abs_wq_df.loc[keep,'band_1':'band_1024']
+    Y = Y[keep]
+    
+    # name_dum = pd.get_dummies(abs_wq_df['Name'])
+    # filtered_dum = pd.get_dummies(abs_wq_df['Filtered'])
+    # X = abs_wq_df.loc[keep,:]
+    # Y = abs_wq_df.Nitrate[keep].to_numpy()
+    # name_dum = pd.get_dummies(abs_wq_df['Name'][keep])
+    # X = pd.concat([name_dum,X],axis=1).to_numpy()
+    # X = name_dum
+    
+    
+    X_train, X_test, y_train, y_test = train_test_split(X, Y, random_state=rs,
+                                                        test_size = 0.3)
+    # test_names = X_test.Name.reset_index(drop=True)
+    # test_filt = X_test.Filtered.reset_index(drop=True)
+    
+    # X_train = X_train.loc[:,'band_1':'band_1024']
+    # X_test = X_test.loc[:,'band_1':'band_1024']
+    pls = PLSRegression()
+    clf = GridSearchCV(pls,param_grid,scoring = 'neg_mean_absolute_error')
+    # clf = GridSearchCV(pls,param_grid,scoring = 'r2')
+    # clf.cv_results_
+    # clf = GridSearchCV(pls,param_grid)
+    clf.fit(X_train,y_train)
+    n_comp = clf.best_params_['n_components']
+    print('Number of components:\t'+str(n_comp))
+    pls_opt = clf.best_estimator_
+    Y_hat = pls_opt.predict(X_test)
+    Y_hat_train = pls_opt.predict(X_train)
+    
+    r_sq = pls_opt.score(X_test,y_test)
+    print('Test r-squared value:\t'+str(round(r_sq,3)))
+    
+    r_sq_train = pls_opt.score(X_train,y_train)
+    print('Training r-squared value:\t'+str(round(r_sq_train,3)))
+    
+    
+    
+    ### Plot results
+    # plt.plot(Y_hat,y_test,'b.')
+    
+    line11 = np.linspace(min(np.concatenate((y_test,Y_hat[:,0]))),
+                         max(np.concatenate((y_test,Y_hat[:,0]))))
+    
+    # lr = LinearRegression().fit(Y_hat,y_test)
+    # linelr = lr.predict(line11.reshape(-1,1))
+    
+    plt.figure()
+    plt.plot(y_test,Y_hat,'o',markersize = 4, label = 'predictions')
+    plt.plot(line11,line11,label= '1:1 line')
+    # plt.plot(line11,linelr,label = 'regression line')
+    plt.xlabel('Lab Measured Nitrate (mg/L)')
+    plt.ylabel('Predicted Nitrate (mg/L)')
+    plt.text(170,200,r'$r^2 =$'+str(np.round(r_sq,3)))
+    plt.title('Test Set')
+    plt.legend()
+    plt.show()
+    
+    line11 = np.linspace(min(np.concatenate((y_train,Y_hat_train[:,0]))),
+                         max(np.concatenate((y_train,Y_hat_train[:,0]))))
+    
+    # lr = LinearRegression().fit(Y_hat,y_test)
+    # linelr = lr.predict(line11.reshape(-1,1))
+    
+    plt.plot(y_train,Y_hat_train,'o',markersize = 4, label = 'predictions')
+    plt.plot(line11,line11,label= '1:1 line')
+    # plt.plot(line11,linelr,label = 'regression line')
+    plt.title('Training Set')
+    plt.xlabel('Lab Measured Nitrate (mg/L)')
+    plt.ylabel('Predicted Nitrate (mg/L)')
+    plt.text(0.8*max(line11),min(line11),r'$r^2 =$'+str(np.round(r_sq_train,3)))
+    plt.legend()
+    plt.show()
 
 #%%
 
@@ -215,33 +222,66 @@ plt.text(0.5,2,r'$r^2 =$'+str(np.round(r_sq,3)))
 #%% 
 ### Create a model for every species
 s = 'Molybdenum'
-# y_hat_test_df = pd.DataFrame(columns=species)
-y_hat_test_dict = dict()
-y_hat_train_dict = dict()
-# y_true_train_dict = dict()
-# y_true_test_dict = dict()
-test_ind_dict = dict()
-train_ind_dict = dict()
-test_rsq_dict = dict()
-train_rsq_dict = dict()
-test_rmse_dict = dict()
-train_rmse_dict = dict()
-test_mape_dict = dict()
-train_mape_dict = dict()
-n_comp_dict = dict()
 
-for s in species:
-    y_hat_test_dict[s]=[]
-    y_hat_train_dict[s] = []
-    test_ind_dict[s] = []
-    train_ind_dict[s] = []
-    test_rsq_dict[s] = []
-    train_rsq_dict[s] = []
-    test_rmse_dict[s] = []
-    train_rmse_dict[s] = []
-    test_mape_dict[s] = []
-    train_mape_dict[s] = []
-    n_comp_dict[s] = []
+outputs_df = pd.DataFrame(columns= ['output','species','iteration','value']) #save outputs in dataframe
+
+output_names = ['y_hat_test','y_hat_train','y_true_train','y_true_test',
+                'test_ind','train_ind','test_rsq','train_rsq','test_rmse',
+                'train_rmse','test_mape','train_mape','n_comp']
+
+variable_names = ['Y_hat','Y_hat_train','list(y_train)', 'list(y_test)',
+                  'list(X_test.index)','list(X_train.index)','r_sq','r_sq_train','RMSE_test',
+                  'RMSE_train','MAPE_test','MAPE_train','n_comp']
+
+# for vn in variable_names: # this is for testing
+#     print(vn+':',type(eval(vn)))
+
+
+## save outputs in dictionaries
+# y_hat_test_dict = dict()
+# y_hat_train_dict = dict()
+# # y_true_train_dict = dict()
+# # y_true_test_dict = dict()
+# test_ind_dict = dict()
+# train_ind_dict = dict()
+# test_rsq_dict = dict()
+# train_rsq_dict = dict()
+# test_rmse_dict = dict()
+# train_rmse_dict = dict()
+# test_mape_dict = dict()
+# train_mape_dict = dict()
+# n_comp_dict = dict()
+
+# for s in species:
+#     y_hat_test_dict[s]=[]
+#     y_hat_train_dict[s] = []
+#     test_ind_dict[s] = []
+#     train_ind_dict[s] = []
+#     test_rsq_dict[s] = []
+#     train_rsq_dict[s] = []
+#     test_rmse_dict[s] = []
+#     train_rmse_dict[s] = []
+#     test_mape_dict[s] = []
+#     train_mape_dict[s] = []
+#     n_comp_dict[s] = []
+
+def write_output_df(the_output,output_name,species_name,iteration_num):
+    
+    if isinstance(the_output,float):
+        sub_df = pd.DataFrame([[output_name,species_name,iteration_num,the_output]],
+                                       columns= ['output','species','iteration','value'])
+    elif isinstance(the_output,list):
+        sub_df = pd.DataFrame(columns= ['output','species','iteration','value'])
+        sub_df['value']=the_output
+        sub_df['output']=output_name
+        sub_df['species']=species_name
+        sub_df['iteration']=iteration_num
+    else:
+        print('Error: outputs must be of type list or float')
+    return(sub_df)
+
+iteration = 1 # this is for testing
+
 
 for s in species:
     Y = abs_wq_df[s]
@@ -271,39 +311,54 @@ for s in species:
     # clf.cv_results_
     # clf = GridSearchCV(pls,param_grid)
     clf.fit(X_train,y_train)
-    n_comp = clf.best_params_['n_components']
+    n_comp = float(clf.best_params_['n_components'])
     pls_opt = clf.best_estimator_
-    Y_hat = pls_opt.predict(X_test)[:,0]
-    Y_hat_train = pls_opt.predict(X_train)[:,0]
+    Y_hat = list(pls_opt.predict(X_test)[:,0])
+    Y_hat_train = list(pls_opt.predict(X_train)[:,0])
     
-    r_sq = pls_opt.score(X_test,y_test)
-    r_sq_train = pls_opt.score(X_train,y_train)
+    r_sq = float(pls_opt.score(X_test,y_test))
+    r_sq_train = float(pls_opt.score(X_train,y_train))
     
     MSE_test = MSE(y_test,Y_hat)
-    RMSE_test = np.sqrt(MSE_test)
+    RMSE_test = float(np.sqrt(MSE_test))
     
     MSE_train = MSE(y_train,Y_hat_train)
-    RMSE_train = np.sqrt(MSE_train)
+    RMSE_train = float(np.sqrt(MSE_train))
     
     abs_test_errors = abs(y_test-Y_hat)
     APE_test = abs_test_errors/y_test # APE = absolute percent error,decimal
-    MAPE_test = np.mean(APE_test)*100 # this is percentage
+    MAPE_test = float(np.mean(APE_test)*100) # this is percentage
     
     abs_train_errors = abs(y_train-Y_hat_train)
     APE_train = abs_train_errors/y_train # APE = absolute percent error,decimal
-    MAPE_train = np.mean(APE_train)*100 # this is percentage
+    MAPE_train = float(np.mean(APE_train)*100) # this is percentage
     
-    y_hat_test_dict[s].extend(list(Y_hat))
-    y_hat_train_dict[s].extend(list(Y_hat_train))
-    test_ind_dict[s].extend(list(X_test.index))
-    train_ind_dict[s].extend(list(X_train.index))
-    test_rsq_dict[s].append(float(r_sq))
-    train_rsq_dict[s].append(float(r_sq_train))
-    test_rmse_dict[s].append(float(RMSE_test))
-    train_rmse_dict[s].append(float(RMSE_train))
-    test_mape_dict[s].append(float(MAPE_test))
-    train_mape_dict[s].append(float(MAPE_train))
-    n_comp_dict[s].append(float(n_comp))
+    
+    # y_hat_test_dict[s].extend(list(Y_hat))
+    # y_hat_train_dict[s].extend(list(Y_hat_train))
+    # test_ind_dict[s].extend(list(X_test.index))
+    # train_ind_dict[s].extend(list(X_train.index))
+    # test_rsq_dict[s].append(float(r_sq))
+    # train_rsq_dict[s].append(float(r_sq_train))
+    # test_rmse_dict[s].append(float(RMSE_test))
+    # train_rmse_dict[s].append(float(RMSE_train))
+    # test_mape_dict[s].append(float(MAPE_test))
+    # train_mape_dict[s].append(float(MAPE_train))
+    # n_comp_dict[s].append(float(n_comp))
+    
+    # sub_df = pd.DataFrame(columns= ['output','species','iteration','value'])
+    # sub_df['value']=r_sq
+    # sub_df['output']='test_rsq'
+    # sub_df['species']=s
+    # sub_df['iteration']=iteration
+        
+    # sub_df = write_output_df(r_sq, 'test_rsq', s, iteration)
+    # outputs_df = outputs_df.append(sub_df,ignore_index=True)
+    
+    for out in range(len(output_names)):
+        print(out)
+        sub_df = write_output_df(eval(variable_names[out]), output_names[out], s, iteration)
+        outputs_df = outputs_df.append(sub_df,ignore_index=True)
     
     # plt.plot(Y_hat,y_test,'b.')
     
@@ -350,14 +405,62 @@ for s in species:
     # plt.legend()
     # plt.show()
     
+#%%
+
+## make plots
+    
 fig, axs = plt.subplots(4,4)
 fig.set_size_inches(12.8,9.6)
+axs[3, 2].axis('off')
+axs[3, 3].axis('off')
 row = 0
 col = 0
 for s in species:
-    y_true_train = abs_wq_df.loc[train_ind_dict[s],s]
-    y_hat_train = y_hat_train_dict[s]
-    axs[row,col].plot(y_true_train,y_hat_train)
+    y_true_train = outputs_df.loc[((outputs_df.species == s) &
+                                    (outputs_df.output == 'y_true_train')),
+                                   'value']
+    
+    y_hat_train = outputs_df.loc[((outputs_df.species == s) &
+                                    (outputs_df.output == 'y_hat_train')),
+                                   'value']
+    
+    y_true_test = outputs_df.loc[((outputs_df.species == s) &
+                                    (outputs_df.output == 'y_true_test')),
+                                   'value']
+    
+    y_hat_test = outputs_df.loc[((outputs_df.species == s) &
+                                    (outputs_df.output == 'y_hat_test')),
+                                   'value']
+    
+    line11 = np.linspace(min(np.concatenate((y_true_train,y_hat_train,
+                                             y_true_test,y_hat_test))),
+                          max(np.concatenate((y_true_train,y_hat_train,
+                                             y_true_test,y_hat_test))))
+    
+    y_text1 = min(line11)+(max(line11)-min(line11))*0.05
+    y_text2 = min(line11)+(max(line11)-min(line11))*0.15
+    x_text = max(line11)-(max(line11)-min(line11))*0.3
+    
+    # lr = LinearRegression().fit(Y_hat,y_test)
+    # linelr = lr.predict(line11.reshape(-1,1))
+    
+    # plt.plot(y_true_train,y_hat_train,'o',markersize = 4, label = 'predictions')
+    # plt.plot(line11,line11,label= '1:1 line')
+    # plt.title(s)
+    # # plt.plot(line11,linelr,label = 'regression line')
+    # plt.xlabel('Lab Measured '+s+' (mg/L)')
+    # plt.ylabel('Predicted '+s+' (mg/L)')
+    # plt.text(x_text,y_text1,r'$r^2 =$'+str(np.round(r_sq,3)))
+    # plt.text(x_text,y_text2,r'MAPE = '+str(np.round(MAPE_test,1))+'%')
+    # plt.legend()
+    # plt.show()
+    
+    axs[row,col].plot(y_true_train,y_hat_train,'o',markersize = 4, label = 'training set')
+    axs[row,col].plot(y_true_test,y_hat_test,'o',markersize = 4, label = 'test set')
+    axs[row,col].plot(line11,line11,'k--',label= '1:1 line')
+    axs[row,col].set_title(s)
+    axs[row,col].legend()
+    axs[row,col].get_xaxis().set_visible(False)
     if col == 3:
         col = 0
         row += 1
@@ -369,6 +472,16 @@ fig,axs = plt.subplots(2)
 fig.set_size_inches(12.8,9.6)
 for plots in [0,1]:
     axs[plots].plot([1,2],[2,4])
+
+#%%
+my_dict = {}
+my_dict.values
+
+#%%
+my_arr = np.empty((4,4))
+my_arr[:,2] = [1,2,3,4]
+my_arr[:,1] = 'hi'
+
 #%%
 
 ## Random Forest (nitrate)
