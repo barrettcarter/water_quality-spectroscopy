64ae8ac7fcdb3a23437382c2f43983ed8e03fbc5
J. Barrett Carter <barrettcarter1991@gmail.com>
2021-09-13 16:22:01 -0400

    experimented with baseline correction in calc_abs code

--- Code/calc_abs.py
+++ Code/calc_abs.py
@@ -18,8 +18,8 @@ def calc_abs(sample_array,reference_array,baseline_corr = False,
         
     else:
         
-        sample_array = sample_array - min(sample_array)
-        reference_array = reference_array - min(reference_array)
+        sample_array = sample_array - 0.95*min(reference_array)
+        reference_array = reference_array - 0.95*min(reference_array)
         absorbance = np.log10(reference_array/sample_array)
 
     return absorbance
@@ -30,15 +30,15 @@ specDir='C:\\Users\\'+user+'\\OneDrive\\Research\\PhD\\Data\\spectra\\'      # L
 #%%
 ############################### Bring in files
 
-refName = 'spec_ref_0deg_22500.0_UFABE_2021-01-08T10%3A15%3A35.160053.csv'          # reference spectrum
+refName = 'spec_ref_0deg_20700.0_UFABE_2021-09-02T13%3A19%3A30.163411.csv'          # reference spectrum
 refSpec_df = pd.read_csv(specDir+refName)
 waveLengths = pd.DataFrame.to_numpy(refSpec_df.WVL)
 
 ref = refSpec_df.INT
 ref = ref.to_numpy()
 
-sample1Name = 'spec_hat_unfiltered_01-05-2021_0deg_22500.0_UFABE_2021-01-08T10%3A16%3A08.936184.csv'
-sample2Name = 'spec_posnw16_unfiltered_01-05-2021_0deg_22500.0_UFABE_2021-01-08T10%3A30%3A12.475933.csv'
+sample1Name = 'spec_HNSs1d30_False_08-25-2021_0deg_20700.0_UFABE_2021-09-02T13%3A25%3A09.795173.csv'
+sample2Name = 'spec_HNSs2d30_False_08-30-2021_0deg_20700.0_UFABE_2021-09-02T13%3A31%3A05.583398.csv'
 
 sample1 = pd.read_csv(specDir+sample1Name)
 sample1 = sample1.INT
@@ -60,17 +60,17 @@ abs_2_corr = calc_abs(sample2,ref,wavelengths = waveLengths,baseline_corr=True)
 
 ### make plots
 plt.figure()
-plt.plot(waveLengths[0:300],abs_1[0:300],label = 'hat unfiltered')
-plt.plot(waveLengths[0:300],abs_2[0:300],label = 'posnw16 unfiltered')
+plt.plot(waveLengths[0:300],abs_1[0:300],label = 'HNSs1d30')
+plt.plot(waveLengths[0:300],abs_2[0:300],label = 'HNSs2d30')
 plt.title('original (uncorrected)')
 plt.xlabel('wavelength (nm)')
 plt.ylabel('absorbance')
 plt.legend(loc='upper right')
 
 plt.figure()
-plt.plot(waveLengths[0:300],abs_1_corr[0:300],label = 'hat unfiltered')
-plt.plot(waveLengths[0:300],abs_2_corr[0:300],label = 'posnw16 unfiltered')
-plt.ylim(0,1.75)
+plt.plot(waveLengths[0:300],abs_1_corr[0:300],label = 'HNSs1d30')
+plt.plot(waveLengths[0:300],abs_2_corr[0:300],label = 'HNSs2d30')
+plt.ylim(0,3)
 plt.title('baseline corrected')
 plt.xlabel('wavelength (nm)')
 plt.ylabel('absorbance')
@@ -79,8 +79,8 @@ plt.legend(loc='upper right')
 
 
 plt.figure()
-plt.plot(waveLengths,sample1,label = 'hat unfiltered')
-plt.plot(waveLengths,sample2,label = 'posnw16 unfiltered')
+plt.plot(waveLengths,sample1,label = 'HNSs1d30')
+plt.plot(waveLengths,sample2,label = 'HNSs2d30')
 plt.plot(waveLengths,refSpec_df.INT,label = 'reference')
 plt.title('spectra')
 plt.xlabel('wavelength (nm)')
