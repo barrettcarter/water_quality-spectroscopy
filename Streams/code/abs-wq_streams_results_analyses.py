b9ba82b9fc3dbc2c6d6a818a980ab3a4a9d70fb1
J. Barrett Carter <barrettcarter1991@gmail.com>
2022-01-13 17:09:27 -0500

    Update abs-wq_streams_results_analyses.py
    added code for nRMSE and NSE

--- Streams/code/abs-wq_streams_results_analyses.py
+++ Streams/code/abs-wq_streams_results_analyses.py
@@ -54,9 +54,15 @@ results_dl_df['model'] = 'dl'
 results_df = pd.concat([results_pls_df,results_dl_df],ignore_index=True)
 results_df.loc[:,'value']=results_df.loc[:,'value'].apply(lambda x: float(x))
 
+#%% make some useful variables
+
 species = results_df['species'].unique()
 species = list(species)
 species.sort(key = lambda x: x[-1])
+
+iterations = results_df.iteration.unique()
+
+models = results_df.model.unique()
 
 #%% get rmse values
 
@@ -87,3 +93,125 @@ fit_plot_df['Species']=y_hat_tests.species
 
 sns.relplot(data = fit_plot_df, x = 'Predicted Concentration',y = 'True Concentration',
             hue = 'Model', col = 'Species',col_wrap = 3)
+
+#%% make normalized rmses
+
+rmses_norm_test = test_rmses.copy()
+rmses_norm_test.rename(columns = {'test rmse (ppm)':'value'},inplace = True)
+
+# s, i, m = species[0], iterations [0], models[0]
+
+for s in species:
+    for i in iterations:
+        for m in models:
+            
+            row = (rmses_norm_test.species == s) \
+                & (rmses_norm_test.iteration == i) \
+                    & (rmses_norm_test.model == m)
+            
+            rows = (y_true_tests.species == s) \
+                & (y_true_tests.iteration == i) \
+                    & (y_true_tests.model == m)
+            
+            rmse = rmses_norm_test.loc[row,'value']
+            av = y_true_tests.loc[rows,'value'].mean()
+            rmses_norm_test.loc[row,'value'] = rmse/av
+            
+
+
+#%% save average normalized test rmses
+
+rmses_norm_test_av = pd.DataFrame(columns= ['model','species','value'])
+
+s, i, m = species[0], iterations [0], models[0]
+
+for s in species:
+    for m in models:
+        
+        rows = (rmses_norm_test.species == s) & (rmses_norm_test.model == m)
+            
+        rmse_norm_av = rmses_norm_test.loc[rows,'value'].mean()
+        
+        new_row = pd.DataFrame([[m,s,rmse_norm_av]],columns=['model','species','value'])
+        
+        rmses_norm_test_av = rmses_norm_test_av.append(new_row)
+
+#%% calculate overall average normalized rmse
+
+rmse_av_norm = rmses_norm_test_av.value.mean()
+                
+#%% make plot of normalized rmses
+
+rmses_norm_test.rename(columns = {'value':'normalized test rmse'},inplace = True)
+norm_test_rmse_plot = sns.catplot(x='model',y='normalized test rmse',
+                                  col = 'species',col_wrap=3,
+                             data = rmses_norm_test,kind = 'violin')
+rmses_norm_test.rename(columns = {'normalized test rmse (ppm)':'value'},inplace = True)  
+
+#%% calculate NSEs
+
+NSEs_test = test_rmses.copy()
+NSEs_test.rename(columns = {'test rmse (ppm)':'value'},inplace = True)
+
+s, i, m = species[0], iterations [0], models[0]
+
+for s in species:
+    for i in iterations:
+        for m in models:
+            
+            row = (NSEs_test.species == s) \
+                & (NSEs_test.iteration == i) \
+                    & (NSEs_test.model == m)
+            
+            rows = (y_true_tests.species == s) \
+                & (y_true_tests.iteration == i) \
+                    & (y_true_tests.model == m)
+            
+            y_hats = y_hat_tests.loc[rows,'value']
+            y_trues = y_true_tests.loc[rows,'value']
+            
+            errors = (y_hats-y_trues)
+            sq_errors = errors*errors
+            
+            sse = sq_errors.sum()
+            
+            av = y_true_tests.loc[rows,'value'].mean()
+            
+            errors = (av-y_trues)
+            sq_errors = errors*errors
+            
+            sse_av = sq_errors.sum()
+            
+            nse = 1 - sse/sse_av
+
+            NSEs_test.loc[row,'value'] = nse
+
+#%% make plot of NSEs without ammonium (too large of range)
+
+NSEs_test.rename(columns = {'value':'NSE'},inplace = True)
+NSEs_test_plot = sns.catplot(x='model',y='NSE',
+                                  col = 'species',col_wrap=3,
+                             data = NSEs_test.loc[NSEs_test.species != 'Ammonium-N',:],
+                             kind = 'violin')
+NSEs_test.rename(columns = {'NSE':'value'},inplace = True)  
+
+#%% save average NSEs
+
+NSEs_test_av = pd.DataFrame(columns= ['model','species','value'])
+
+# s, i, m = species[0], iterations [0], models[0]
+
+for s in species:
+    for m in models:
+        
+        rows = (NSEs_test.species == s) & (NSEs_test.model == m)
+            
+        NSE_av = NSEs_test.loc[rows,'value'].mean()
+        
+        new_row = pd.DataFrame([[m,s,NSE_av]],columns=['model','species','value'])
+        
+        NSEs_test_av = NSEs_test_av.append(new_row)
+
+#%% calculate overall average normalized rmse
+
+NSE_test_av = NSEs_test_av.value.mean()
