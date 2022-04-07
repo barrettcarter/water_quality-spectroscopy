ad4d30ec23239886f27fcfe597ee2eb64395abe3
J. Barrett Carter <barrettcarter1991@gmail.com>
2022-04-07 10:31:09 -0400

    Update abs-wq_an_HNSr_d30.py

--- Hydroponics/code/abs-wq_an_HNSr_d30.py
+++ Hydroponics/code/abs-wq_an_HNSr_d30.py
@@ -26,89 +26,21 @@ from sklearn.ensemble import RandomForestRegressor
 import sklearn.metrics
 sorted(sklearn.metrics.SCORERS.keys())
 
-#%%
-
-### Set paths and bring in data
+#%% Set paths and bring in data
 
 user = os.getlogin() 
 path_to_wqs = 'C:\\Users\\'+user+'\\OneDrive\\Research\\PhD\\Data_analysis\\water_quality-spectroscopy\\'
-abs_df_dir=os.path.join(path_to_wqs,'Data/spectra/')
-wq_df_dir=os.path.join(path_to_wqs,'Hydroponics/inputs/water_quality/')
+inter_dir=os.path.join(path_to_wqs,'Hydroponics/intermediates/')
 
-
-wq_df_fn = 'wq_HNSr_df.csv'
-abs_df_fn = 'abs_df_u2d.csv'
+abs_wq_df_fn = 'abs-wq_HNSrd30_df.csv'
 
 # Bring in data
-abs_df=pd.read_csv(abs_df_dir+abs_df_fn)
-wq_df=pd.read_csv(wq_df_dir+wq_df_fn)
+abs_wq_df=pd.read_csv(inter_dir+abs_wq_df_fn)
 
-#%%
+#%% Some useful variables
 
-### Some data wrangling
-
-# Select only HNSr samples
-
-abs_df = abs_df.loc[(abs_df.Name == 'HNSr1') | (abs_df.Name == 'HNSr2')\
-                    | (abs_df.Name == 'HNSr3'),:]
-abs_df = abs_df.reset_index(drop = True)
-abs_df.loc[7,'Name']='HNSr1' # this sample was labelled incorrectly (probably)
-# Make species names consistent
-
-wq_df.Species.unique()
-
-wq_df.Species[wq_df.Species=='Ammonia-Nitrogen']='Ammonium-N'
-wq_df.Species[wq_df.Species=='Nitrate-Nitrogen']='Nitrate-N'
-
-# Makes dates match
-
-wq_dates = wq_df.Date_col.unique()
-print(wq_dates)
-abs_dates = abs_df.Date_col.unique()
-print(abs_dates)
-# wq_df['Date_col'][wq_df.Date_col=='5/3/2021']='5/4/2021'
-# wq_dates = wq_df.Date_col.unique()
-
-# Create sample IDs for combining two dataframes
-# wq_cols = list(wq_df.columns[0:4])
-# wq_cols.append('Name')
-# wq_df.columns=wq_cols
-
-wq_df.rename(columns={'Sample_num':'Name'},inplace=True)
-wq_df['ID']=wq_df.Name+wq_df.Date_col
-
-# concs_ind = range(int((wq_df.shape[0]-1)/3))
-# concs_df = wq_df.pivot(index = range(268),columns = 'Species',values = 'Conc')
-
-#%%
-
-### Make dataframe with absorbances and water quality
-
-species = wq_df.Species.unique()
-species = np.append(species,'ID')
-aw_df_cols = np.append(species,abs_df.columns)
-abs_wq_df = abs_df
-
-abs_wq_df.loc[:,species] = -0.1
-abs_wq_df['ID']=abs_wq_df.Name+abs_wq_df.Date_col
-
-species = np.delete(species,len(species)-1) #get rid of ID
-species = np.delete(species,len(species)-1) #get rid of Conductivity
-
-abs_wq_df = abs_wq_df[aw_df_cols]
-
-for wq_row in range(wq_df.shape[0]):
-    for abs_row in range(abs_wq_df.shape[0]):
-        if wq_df.ID[wq_row]==abs_wq_df.ID[abs_row]:
-            for s in species:
-                if wq_df.Species[wq_row] == s:
-                    abs_wq_df.loc[abs_row,s]=wq_df.Value[wq_row]
                              
-#%%
-#################################################################
-
-
-### Tuning the models
+#%% Tuning the model for single species
 
 ## Nitrate
 ## PlSR
@@ -282,6 +214,7 @@ def write_output_df(the_output,output_name,species_name,iteration_num):
 
 iteration = 1 # this is for testing
 
+species = abs_wq_df.columns[1:14]
 
 for s in species:
     Y = abs_wq_df[s]
