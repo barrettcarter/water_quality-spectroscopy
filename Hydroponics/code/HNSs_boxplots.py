7c52cf76997288f88eea658d02b15417938a9fa4
J. Barrett Carter <barrettcarter1991@gmail.com>
2022-10-20 13:39:46 -0400

    Create HNSs_boxplots.py

---
+++ Hydroponics/code/HNSs_boxplots.py
@@ -0,0 +1,59 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Oct 20 09:25:40 2022
+
+@author: jbarrett.carter
+"""
+#%% Import libraries
+import os
+import numpy as np
+import pandas as pd
+from matplotlib import pyplot as plt
+import matplotlib
+
+#%% Bring in Data
+
+user = os.getlogin() 
+path_to_wqs = 'C:\\Users\\'+user+'\\OneDrive\\Research\\PhD\\Data_analysis\\water_quality-spectroscopy\\'
+wq_path = os.path.join(path_to_wqs,'Hydroponics/inputs/water_quality/')
+wq_fn = 'wq_HNSs_df.csv'
+data_path = os.path.join(wq_path,wq_fn)
+HNSs_df=pd.read_csv(data_path)
+
+#%% Determine data ranges
+
+HNSs_stats = HNSs_df.describe()
+
+#%% Define Groups
+
+group1 = ['NO3N','K','Ca']
+group2 = ['S','P','Mg']
+group3 = ['NH4N','pH','Fe']
+group4 = ['Mn','B','Zn','Cu','Mo']
+
+g1_df = HNSs_df.loc[:,group1]
+g2_df = HNSs_df.loc[:,group2]
+g3_df = HNSs_df.loc[:,group3]
+g4_df = HNSs_df.loc[:,group4]
+
+#%% Make plots
+matplotlib.rcParams.update({'font.size': 14})
+plt.figure()
+plt.boxplot(g1_df,notch=True,medianprops = {'color': 'red'})
+plt.ylabel('Concentration (mg/L)')
+plt.xticks([1,2,3],group1)
+
+plt.figure()
+plt.boxplot(g2_df,notch=True,medianprops = {'color': 'red'})
+plt.ylabel('Concentration (mg/L)')
+plt.xticks([1,2,3],group2)
+
+plt.figure()
+plt.boxplot(g3_df,notch=True,medianprops = {'color': 'red'})
+plt.ylabel('Concentration (mg/L)')
+plt.xticks([1,2,3],group3)
+
+plt.figure()
+plt.boxplot(g4_df,notch=True,medianprops = {'color': 'red'})
+plt.ylabel('Concentration (mg/L)')
+plt.xticks([1,2,3,4,5],group4)
