81d8b9016563d39676907cff5fda92a4d52ab6d1
J. Barrett Carter <barrettcarter1991@gmail.com>
2023-07-31 07:27:48 -0400

    Update DL_mod_import.py
    Fixed issue with model import by adding code for lenet class to script.

--- Streams/code/DL_mod_import.py
+++ Streams/code/DL_mod_import.py
@@ -21,6 +21,197 @@ from matplotlib.axis import Tick
 # from scipy import stats
 # import seaborn as sns
 
+#%% Define class for pre-processing data and training neural network.
+
+def augment(x, betashift, slopeshift, multishift):
+    beta = np.random.random(size=(x.shape[0],1))*2*betashift-betashift
+    slope = np.random.random(size=(x.shape[0],1))*2*slopeshift-slopeshift + 1
+    axis = np.array(range(x.shape[1]))/float(x.shape[1])
+    offset = slope*(axis) + beta - axis - slope/2 + 0.5
+    multi = np.random.random(size=(x.shape[0],1))*2*multishift-multishift + 1
+    return(multi*x + offset)
+
+def add_dimension(ds_x):
+    
+    return(np.reshape(ds_x, (ds_x.shape[0], 1, ds_x.shape[1])))
+
+class lenet():
+    
+    def __init__(self):
+        
+        self.ds_x_smooth = 0
+
+        
+    def prepare_x(self, x_df, augmentRatio=10, smooth = False):
+        
+        self.x_df = x_df
+        
+        self.smooth = smooth
+        
+        self.augmentRatio = augmentRatio
+        
+        # make scaled absorbance dataset for training
+        self.scaler_x = MinMaxScaler()
+        self.ds_x = self.x_df.values
+        # pca = PCA(n_components = 10)
+        # ds_x = pca.fit_transform(ds_x)
+        self.scaler_x = self.scaler_x.fit(self.ds_x)
+
+        self.ds_x = self.scaler_x.transform(self.ds_x)
+        
+        """## Smoothing
+        Using Savitzky-Golay
+        """
+
+        if self.smooth == True:
+        
+            window = 5
+            degree = 3
+            
+            self.x = np.array(list(map(lambda s: float(s[4:].replace('_', '.')), self.df.columns[6:])))
+            
+            self.ds_x_smooth = np.array(list(map(lambda y: savgol_filter((self.x, y),
+                                                                         window, degree)[1], self.ds_x)))
+             
+        else:
+            
+            self.ds_x_smooth=self.ds_x
+
+        """"DROPPING OUT EVERY N'th WAVELENGTH"""
+        self.n_drop = 4
+        self.ds_x_smooth = self.ds_x_smooth[:,range(0,self.ds_x_smooth.shape[1],self.n_drop)]
+        
+        # final step to prepare for using in Sequential() model
+        
+        self.X_train = self.ds_x_smooth
+        
+        self.X_train_aug = self.X_train.repeat(self.augmentRatio, axis=0)
+        self.X_train_aug = augment(self.X_train_aug, .01, .01, .01)
+        
+        self.X_train = add_dimension(self.X_train)
+        self.X_train_aug = add_dimension(self.X_train_aug)
+        
+        return(self.X_train)
+        
+    
+    def transform_x(self,x_new_df):
+        
+        if type(self.ds_x_smooth) == int:
+            
+            print('ERROR: x must be prepared first by using prepare_x method.')
+        
+        self.x_new_df = x_new_df
+
+        self.x_new_df = self.scaler_x.transform(self.x_new_df)
+        
+        """## Smoothing
+        Using Savitzky-Golay
+        """
+
+        if self.smooth == True:
+        
+            window = 5
+            degree = 3
+            
+            self.x_new_df = np.array(list(map(lambda y: savgol_filter((self.x, y),
+                                                                         window, degree)[1], self.x_new_df)))
+
+        """"DROPPING OUT EVERY N'th WAVELENGTH"""
+        self.x_new_df = self.x_new_df[:,range(0,self.x_new_df.shape[1],self.n_drop)]
+        
+        # final step to prepare for using in Sequential() model
+        
+        self.x_new_df = add_dimension(self.x_new_df)
+        
+        return(self.x_new_df)
+        
+    # def plot_x(self):
+
+    #     plt.figure(figsize=(12, 8))
+    #     for i in range(self.ds_x_smooth.shape[0]):
+    #         plt.plot(self.ds_x_smooth[i])
+    #     plt.xlabel('Wavelength (nm)')
+    #     plt.ylabel('Absorbanced (Normalized)')
+    #     plt.show()
+            
+        
+    # def smooth_plot(self):
+        
+    #     if self.smooth == True:
+    
+    #         plt.figure(figsize=(12, 8))
+    #         plt.plot(self.x, self.ds_x[2])
+    #         plt.plot(self.x, self.ds_x_smooth[2])
+    #         plt.legend(['Original', 'Smooth'])
+    #         plt.xlabel('Wavelength (nm)')
+    #         plt.ylabel('Absorbance (Normalized)')
+    #         plt.title(f'Savitzky-Golay Smoothing (Window Size={window}, Degree={degree})')
+    #         plt.show()
+            
+    #     else:
+            
+    #         print('x was not smoothed.')
+            
+        
+    def make_model(self, y_train, y_val, X_val, num_epochs = 1000):
+        
+        """## Augmentation"""
+        
+        if type(self.ds_x_smooth) == int:
+            
+            print('ERROR: x must be prepared first by using prepare_x method.')
+        
+        self.y_train = y_train
+        
+        self.y_val = y_val
+        
+        self.X_val = X_val
+        
+        self.num_epochs = num_epochs
+
+        self.multiplier = self.augmentRatio
+    
+        self.y_train = self.y_train.to_numpy()
+        self.y_train.shape=(len(self.y_train),1)
+        
+        self.y_train_aug = self.y_train.repeat(self.multiplier, axis=0)
+        
+        
+        
+        """# Training Creation
+        
+        ## LeNet 5
+        """
+        
+        drop = 0.05
+        reg = .01
+        
+        self.model = Sequential()
+        self.model.add(Conv1D(6, 1,activation='selu',input_shape=(1, self.X_train_aug.shape[2])))
+        self.model.add(Dropout(drop))
+        self.model.add(AveragePooling1D(1))
+        self.model.add(Conv1D(16, 1, activation='selu', kernel_regularizer=l2(reg), bias_regularizer=l2(reg)))
+        self.model.add(Dropout(drop))
+        self.model.add(Flatten())
+        self.model.add(Dense(120, activation='selu', kernel_regularizer=l2(reg), bias_regularizer=l2(reg)))
+        self.model.add(Dropout(drop))
+        self.model.add(Dense(84, activation='selu', kernel_regularizer=l2(reg), bias_regularizer=l2(reg)))
+        self.model.add(Dropout(drop))
+        self.model.add(Dense(1,activation = 'linear'))
+        self.model.compile(loss='mean_squared_error', optimizer=Adam(.001))
+        self.history = self.model.fit(
+            self.X_train_aug, self.y_train_aug,
+            epochs=self.num_epochs, batch_size=int(1e10),
+            verbose=0,
+            validation_data=(self.X_val, self.y_val),
+        )
+        
+        print('Model trained.')
+        
+    def predict(self, x_pred):
+        
+        return(self.model.predict(x_pred).flatten())
+
 #%% Set paths and bring in models
 
 user = os.getlogin() 
@@ -54,6 +245,9 @@ Y = Y[keep]
 X_train, X_test, y_train, y_test = train_test_split(X, Y, random_state=iteration,
                                                                 test_size = 0.3)
 
+X_train = clf.transform_x(X_train)
+X_test = clf.transform_x(X_test)
+
 mod = clf
 
 y_hat = mod.predict(X_test)
@@ -62,45 +256,3 @@ plt.figure()
 plt.scatter(y_test,y_hat)
 plt.ylabel('Predicted')
 plt.xlabel('True')
-
-#%% Look at model tuning parameters
-
-tuning_results = clf.cv_results_
-scores = np.array(tuning_results['mean_test_score']*-1)
-learning_rate = np.array(tuning_results['param_learning_rate'])
-max_depth = np.array(tuning_results['param_max_depth'])
-
-#%% Make plots
-
-fig, axs = plt.subplots(1,2)
-axs[0].plot(learning_rate,scores,'o')
-axs[0].set_xlabel('learning_rate (eta)')
-axs[0].set_ylabel('MAE (mg/L)')
-
-axs[1].plot(max_depth,scores,'o')
-axs[1].set_xlabel('maximum tree depth')
-w = axs[1].get_yaxis() 
-     
-Tick.set_visible(w, False)
-
-#plt.scatter(learning_rate,scores)
-
-#%% test for significance
-
-X_lm = pd.DataFrame({'learning_rate':learning_rate,'max_depth':max_depth})
-X_lm['learning_rate']=X_lm['learning_rate'].apply(lambda x: float(x))
-X_lm['max_depth']=X_lm['max_depth'].apply(lambda x: float(x))
-X_lm = sm.add_constant(X_lm)
-X_lm['axm']=X_lm.learning_rate*X_lm.max_depth
-
-y = pd.DataFrame({'scores':scores})
-
-lr_1 = sm.OLS(y, X_lm).fit()
-
-print(lr_1.summary())
-
-#%%
-plt.figure()
-plt.scatter(learning_rate*max_depth,scores)
-plt.figure()
-plt.scatter(learning_rate,max_depth)
