5e3f45f8940212abcaa76b3495e8abe0196165c3
J. Barrett Carter <barrettcarter1991@gmail.com>
2023-09-27 08:10:14 -0400

    Update output_stats.R
    Finished for loop for combining output files.

--- Code/output_stats.R
+++ Code/output_stats.R
@@ -7,24 +7,36 @@
 
 #### Bring in data
 
-proj_dir = 'C:/Users/barre/Documents/GitHub/water_quality-spectroscopy'
+# proj_dir = 'C:/Users/barre/Documents/GitHub/water_quality-spectroscopy' # For laptop
+
+proj_dir = 'D:/GitHub/PhD/water_quality-spectroscopy' # for work computer
 
 sample_type = 'Hydroponics' # used for defining directories
 
 output_dir = paste(proj_dir, sample_type, 'outputs', sep = '/')
 
-output_files = list.files(output_dir)
+output_files = as.vector(list.files(output_dir))
 
 output_files = output_files[c(17,20,23,24)]
 
 output_files = output_files[c(2,3,4,1)]
 
-outputs_df = read.csv(paste(output_dir,output_files[1], sep = '/'))
+file = output_files[1]
 
-for file in output_files[2:4]:
-  
-  outputs_df = rbind(outputs_df,read.csv(paste(output_dir,file, sep = '/')))
+outputs_df = read.csv(paste(output_dir,file, sep = '/'))
 
-for a in c(1,2,3):
+outputs_df$sample_type = unlist(strsplit(file,'_'))[1]
+
+outputs_df$model = unlist(strsplit(file,'_'))[2]
+
+for (file in output_files[2:4]){
   
-  print(a)
+  output_df = read.csv(paste(output_dir,file, sep = '/'))
+  
+  output_df$sample_type = unlist(strsplit(file,'_'))[1]
+  
+  output_df$model = unlist(strsplit(file,'_'))[2]
+  
+  outputs_df = rbind(outputs_df,output_df)
+  
+}
