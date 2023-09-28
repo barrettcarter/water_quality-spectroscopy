b7e3d6a93f4a8d538e94b284abf3dd99062fea84
J. Barrett Carter <barrettcarter1991@gmail.com>
2023-09-28 08:20:52 -0400

    Update output_stats.R
    Added code for boxplots.

--- Code/output_stats.R
+++ Code/output_stats.R
@@ -5,6 +5,10 @@
 #### The purpose of this code is to perform statistical analysis of ML training
 #### outputs.
 
+### Import libraries
+
+library(ggplot2)
+
 #### Bring in data
 
 # proj_dir = 'C:/Users/barre/Documents/GitHub/water_quality-spectroscopy' # For laptop
@@ -14,6 +18,8 @@ proj_dir = 'D:/GitHub/PhD/water_quality-spectroscopy' # for work computer
 sample_type = 'Hydroponics' # used for defining directories
 
 output_dir = paste(proj_dir, sample_type, 'outputs', sep = '/')
+
+figure_dir = 'C:\\Users\\carter_j\\OneDrive\\Research\\PhD\\Communications\\Images\\HNS results'
 
 output_files = as.vector(list.files(output_dir))
 
@@ -40,3 +46,68 @@ for (file in output_files[2:4]){
   outputs_df = rbind(outputs_df,output_df)
   
 }
+
+outputs = unique(outputs_df$output)
+
+test_rsqs = subset(outputs_df,output=='test_rsq')
+
+p_rsq = ggplot(test_rsqs, aes(x = model, y = value, fill = model)) +
+  geom_boxplot()+
+  scale_fill_brewer(palette = 'Set1')+
+  ylab('test r-sq')+
+  labs(title = 'Undiluted HNS')
+  
+p_rsq
+
+ggsave(filename = 'HNS_rsq_boxplot.png', plot = p_rsq, path = figure_dir, 
+       device = 'png', dpi = 300)
+
+### plot with removed outliers
+
+
+p_rsq_no.outl = ggplot(test_rsqs, aes(x = model, y = value, fill = model)) +
+  geom_boxplot(outlier.shape = NA)+
+  scale_fill_brewer(palette = 'Set1')+
+  ylab('test r-sq')+
+  labs(title = 'Undiluted HNS - outliers removed')+
+  coord_cartesian(ylim = c(-1,1))
+  
+p_rsq_no.outl
+
+ggsave(filename = 'HNS_rsq_boxplot_no-outliers.png', plot = p_rsq_no.outl, path = figure_dir, 
+       device = 'png', dpi = 300)
+
+#### separated by species
+
+p_rsq_sp = ggplot(test_rsqs, aes(x = model, y = value, fill = model)) +
+  geom_boxplot()+
+  facet_wrap(~species, scale = 'free')+
+  scale_fill_brewer(palette = 'Set1')+
+  ylab('test r-sq')+
+  labs(title = 'Undiluted HNS')
+  
+p_rsq_sp
+
+ggsave(filename = 'HNS_rsq-v-species_boxplot.png', plot = p_rsq_sp, path = figure_dir, 
+       device = 'png', dpi = 150, width = 12, height = 10, units = 'in')
+
+### plot with removed outliers
+
+p_rsq_no.outl = ggplot(test_rsqs, aes(x = model, y = value, fill = model)) +
+  geom_boxplot(outlier.shape = NA)+
+  scale_fill_brewer(palette = 'Set1')+
+  ylab('test r-sq')+
+  labs(title = 'Undiluted HNS - outliers removed')+
+  coord_cartesian(ylim = c(-1,1))
+
+p_rsq_no.outl
+
+ggsave(filename = 'HNS_rsq_boxplot_no-outliers.png', plot = p_rsq_no.outl, path = figure_dir, 
+       device = 'png', dpi = 300)
+
+oneway.test(value~model, data = test_rsqs)
+
+
+test_rmses= subset(outputs_df,output=='test_rmse')
+
+
