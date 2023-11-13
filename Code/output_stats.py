712ae1533a8190b0e15d25a7e94922bfff7bb998
J. Barrett Carter <barrettcarter1991@gmail.com>
2023-11-13 11:00:39 -0500

    Update output_stats.py
    Wrote code for making boxplots with tukey groups. Found issue with grouping code, and started to fix. Saved outputs.

--- Code/output_stats.py
+++ Code/output_stats.py
@@ -16,8 +16,38 @@ import matplotlib.pyplot as plt
 import os
 import string
 
+import seaborn as sns
+
+sns.set_theme(style = 'whitegrid',font_scale=1)
+#sns.set(font_scale=2)
+sns.set_style(rc = {'axes.edgecolor':'0.1',
+                      'axes.labelcolor':'0.1',
+                      'grid.linestyle': '--',
+                      'text.color':'0.1',
+                      'xtick.color':'0.1',
+                      'ytick.color':'0.1',
+                      'xtick.direction': 'in',
+                      'ytick.direction': 'in',
+                      'patch.edgecolor': 'w',
+                      'patch.force_edgecolor': True,
+                      'image.cmap': 'Set1',
+                      'font.family': ['sans-serif'],
+                      'font.sans-serif': ['Arial',
+                      'DejaVu Sans',
+                      'Liberation Sans',
+                      'Bitstream Vera Sans',
+                      'sans-serif'],
+                      'xtick.bottom': True,
+                      'xtick.top': True,
+                      'ytick.left': True,
+                      'ytick.right': True,
+                      'axes.spines.left': True,
+                      'axes.spines.bottom': True,
+                      'axes.spines.right': True,
+                      'axes.spines.top': True})
+
 import matplotlib as mpl
-plt.style.use('seaborn-whitegrid')
+# plt.style.use('seaborn-whitegrid')
 
 rc = {'axes.edgecolor':'0.1','axes.labelcolor':'0.1','grid.linestyle': '--',
       'text.color':'0.1','xtick.color':'0.1','ytick.color':'0.1','xtick.direction': 'in',
@@ -37,9 +67,9 @@ for rcparam in rc.keys():
 
 sample_type = 'Hydroponics' # used for navigating directories and other purposes
 
-# proj_dir = 'D:/GitHub/PhD/water_quality-spectroscopy' # for work computer
+proj_dir = 'D:/GitHub/PhD/water_quality-spectroscopy' # for work computer
 
-proj_dir = r'C:\Users\barre\Documents\GitHub\water_quality-spectroscopy' # for laptop
+# proj_dir = r'C:\Users\barre\Documents\GitHub\water_quality-spectroscopy' # for laptop
 
 output_dir = os.path.join(proj_dir, sample_type, 'outputs')
 
@@ -235,7 +265,7 @@ def post_hoc_groups(results_df,factor_cols,block_col,conclusion_col,
         
         block = blocks[0] # for testing
         
-        block = 'Phosphorus' # for testing
+        block = 'Potassium' # for testing
         
         for block in blocks:
             
@@ -247,7 +277,7 @@ def post_hoc_groups(results_df,factor_cols,block_col,conclusion_col,
             
             factor_lev = factor_levs[0] # for testing
             
-            factor_lev = 'PLS' # for testing
+            # factor_lev = 'PLS' # for testing
             
             for factor_lev in factor_levs:
                 
@@ -264,6 +294,12 @@ def post_hoc_groups(results_df,factor_cols,block_col,conclusion_col,
                     sig_pairs_cond = (res_sig_bl[factor_cols].isin(group_fac_levs)).all(axis=1)
                     
                     sig_pairs = res_sig_bl.loc[sig_pairs_cond,factor_cols]
+                    
+                    i = 0
+                    
+                    while sig_pairs.shape[0]>0:
+                        
+                        sig_fac_levs = np.unique(sig_pairs.values).flatten()
                     
                     fac_lev_drop = np.unique(sig_pairs.values).flatten()
                     
@@ -340,7 +376,7 @@ def post_hoc_groups(results_df,factor_cols,block_col,conclusion_col,
                     
     factor_letters['letters'] = factor_letters.letters.apply(lambda x: x[1:])
     
-    # return({'letter_factors':letter_factors,'factor_letters':factor_letters})
+    return({'letter_factors':letter_factors,'factor_letters':factor_letters})
                     
 #%% run grouping function
 
@@ -366,3 +402,103 @@ letter_factors.to_csv(os.path.join(output_dir,'stats',
                                   f'{abbrv}_rsq-rmse_Tukey-by-sp_letter-factors.csv'),
                      index=False)
 
+#%% make variables for plots (should only be run once)
+
+perf_met_dict = perf_mets
+
+perf_mets = list(perf_met_dict.keys())
+
+#%% Make figures
+
+perf_met_labs = {'test_rmse':'log(test RMSE (mg/L))',
+                 'test_rsq':'log(1 - test R-sq (unitless))'}
+
+num_figs = len(species)
+
+fig_dim = int(num_figs**0.5)+1
+
+for perf_met in perf_mets:
+    
+    pm_df = perf_met_dict[perf_met]
+    
+    perf_met_lab = perf_met_labs[perf_met]
+
+    fig, axs = plt.subplots(nrows = fig_dim, ncols = fig_dim, figsize = (16,12),
+                            dpi = 300, sharex = False)
+    
+    plt.subplots_adjust(hspace = 0.2)
+    
+    fig.text(0.5, 0.08, 'ML Algorithm', ha='center', va='center',size=16) # figure x label
+    fig.text(0.08, 0.5, perf_met_lab, ha='center', va='center', 
+             rotation='vertical',size=16) # figure y label
+    
+    row = 0 # for testing
+    col = 0 # for testing
+    
+    i_sp = 0 # required
+    fig_num = 1 # required
+    
+    for row in range(fig_dim):
+        
+        for col in range(fig_dim):
+            
+            ax = axs[row,col]
+            
+            if fig_num > num_figs:
+                
+                ax.axis('off') # turn off extra axes
+                
+                if fig_num == fig_dim**2:
+                    
+                    plt.savefig(os.path.join(figure_dir,f'{abbrv}_{perf_met}_boxplot.png'))
+                
+                fig_num+=1
+                
+                continue
+            
+            sp = species[i_sp]
+        
+            pm_sp = pm_df.loc[pm_df.species==sp,:]
+            
+            pm_sp = pm_sp.sort_values(by = 'model')
+            
+            letters_sp_pm = factor_letters.loc[(factor_letters.species==sp)&
+                                               (factor_letters.perf_met==perf_met),:].reset_index()
+            
+            y_text = pm_sp['trans_val'].max() -\
+                0.1*(pm_sp['trans_val'].max() -\
+                     pm_sp['trans_val'].min())
+                    
+            x_text = [0,1,2,3]
+            
+            text = letters_sp_pm.letters.to_numpy()
+            
+            # sns.violinplot(pm_sp,x = 'model', y = 'trans_val',ax = ax, cut = 0)
+            
+            sns.boxplot(pm_sp,x = 'model', y = 'trans_val',ax = ax)
+            
+            ax.set_xlabel('')
+            ax.set_ylabel('')
+            ax.set_title(sp)
+            
+            if ((row+1)*4 + (col+1)) <= num_figs:
+                
+                ax.set_xticklabels('')
+            
+            for i_text in range(len(text)):
+            
+                ax.text(x_text[i_text],y_text,text[i_text],color = 'red',ha = 'center',
+                        bbox = {'facecolor':'white','alpha':0.7,'boxstyle':'Round, pad=0.1'})
+            
+            i_sp +=1
+            
+            fig_num +=1
+            
+            if col == fig_dim - 1:
+                
+                col = 0
+                row +=1
+            
+            else:
+                
+                col += 1
