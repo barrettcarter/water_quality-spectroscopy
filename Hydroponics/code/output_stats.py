47f125fca94915a90ae65f71e64a99deb93651e9
J. Barrett Carter <barrettcarter1991@gmail.com>
2024-01-07 15:42:53 -0500

    Re-organized files
    Moved output_stats.py and results_plots.py to Hydroponics/code folder. Deleted old R scripts for stats.

--- Hydroponics/code/output_stats.py
+++ Hydroponics/code/output_stats.py
@@ -431,99 +431,100 @@ for perf_met in perf_mets:
     pm_df.to_csv(os.path.join(output_dir,'performance metrics',f'{perf_met}.csv'),index = False)
 
 #%% Make figures
+# This block of code is no longer needed. Use results_plots.py instead.
 
-perf_met_labs = {'test_rmse':'log(test RMSE (mg/L))',
-                 'test_rsq':'log(1 - test R-sq (unitless))'}
+# perf_met_labs = {'test_rmse':'log(test RMSE (mg/L))',
+#                  'test_rsq':'log(1 - test R-sq (unitless))'}
 
-num_figs = len(species)
+# num_figs = len(species)
 
-fig_dim = int(num_figs**0.5)+1
+# fig_dim = int(num_figs**0.5)+1
 
-for perf_met in perf_mets:
+# for perf_met in perf_mets:
     
-    pm_df = perf_met_dict[perf_met]
+#     pm_df = perf_met_dict[perf_met]
     
-    perf_met_lab = perf_met_labs[perf_met]
+#     perf_met_lab = perf_met_labs[perf_met]
 
-    fig, axs = plt.subplots(nrows = fig_dim, ncols = fig_dim, figsize = (16,12),
-                            dpi = 300, sharex = False)
+#     fig, axs = plt.subplots(nrows = fig_dim, ncols = fig_dim, figsize = (16,12),
+#                             dpi = 300, sharex = False)
     
-    plt.subplots_adjust(hspace = 0.2)
+#     plt.subplots_adjust(hspace = 0.2)
     
-    fig.text(0.5, 0.08, 'ML Algorithm', ha='center', va='center',size=16) # figure x label
-    fig.text(0.08, 0.5, perf_met_lab, ha='center', va='center', 
-             rotation='vertical',size=16) # figure y label
+#     fig.text(0.5, 0.08, 'ML Algorithm', ha='center', va='center',size=16) # figure x label
+#     fig.text(0.08, 0.5, perf_met_lab, ha='center', va='center', 
+#              rotation='vertical',size=16) # figure y label
     
-    row = 0 # for testing
-    col = 0 # for testing
+#     row = 0 # for testing
+#     col = 0 # for testing
     
-    i_sp = 0 # required
-    fig_num = 1 # required
+#     i_sp = 0 # required
+#     fig_num = 1 # required
     
-    for row in range(fig_dim):
+#     for row in range(fig_dim):
         
-        for col in range(fig_dim):
+#         for col in range(fig_dim):
             
-            ax = axs[row,col]
+#             ax = axs[row,col]
             
-            if fig_num > num_figs:
+#             if fig_num > num_figs:
                 
-                ax.axis('off') # turn off extra axes
+#                 ax.axis('off') # turn off extra axes
                 
-                if fig_num == fig_dim**2:
+#                 if fig_num == fig_dim**2:
                     
-                    plt.savefig(os.path.join(figure_dir,f'{abbrv}_{perf_met}_boxplot.png'))
+#                     plt.savefig(os.path.join(figure_dir,f'{abbrv}_{perf_met}_boxplot.png'))
                 
-                fig_num+=1
+#                 fig_num+=1
                 
-                continue
+#                 continue
             
-            sp = species[i_sp]
+#             sp = species[i_sp]
         
-            pm_sp = pm_df.loc[pm_df.species==sp,:]
+#             pm_sp = pm_df.loc[pm_df.species==sp,:]
             
-            pm_sp = pm_sp.sort_values(by = 'model')
+#             pm_sp = pm_sp.sort_values(by = 'model')
             
-            letters_sp_pm = factor_letters.loc[(factor_letters.species==sp)&
-                                               (factor_letters.perf_met==perf_met),:].reset_index()
+#             letters_sp_pm = factor_letters.loc[(factor_letters.species==sp)&
+#                                                (factor_letters.perf_met==perf_met),:].reset_index()
             
-            y_text = pm_sp['trans_val'].max() -\
-                0.1*(pm_sp['trans_val'].max() -\
-                     pm_sp['trans_val'].min())
+#             y_text = pm_sp['trans_val'].max() -\
+#                 0.1*(pm_sp['trans_val'].max() -\
+#                      pm_sp['trans_val'].min())
                     
-            x_text = [0,1,2,3]
+#             x_text = [0,1,2,3]
             
-            text = letters_sp_pm.letters.to_numpy()
+#             text = letters_sp_pm.letters.to_numpy()
             
-            # sns.violinplot(pm_sp,x = 'model', y = 'trans_val',ax = ax, cut = 0)
+#             # sns.violinplot(pm_sp,x = 'model', y = 'trans_val',ax = ax, cut = 0)
             
-            sns.boxplot(data = pm_sp,x = 'model', y = 'trans_val',ax = ax)
+#             sns.boxplot(data = pm_sp,x = 'model', y = 'trans_val',ax = ax)
             
-            ax.set_xlabel('')
-            ax.set_ylabel('')
-            ax.set_title(sp)
+#             ax.set_xlabel('')
+#             ax.set_ylabel('')
+#             ax.set_title(sp)
             
-            if ((row+1)*4 + (col+1)) <= num_figs:
+#             if ((row+1)*4 + (col+1)) <= num_figs:
                 
-                ax.set_xticklabels('')
+#                 ax.set_xticklabels('')
             
-            for i_text in range(len(text)):
+#             for i_text in range(len(text)):
             
-                ax.text(x_text[i_text],y_text,text[i_text],color = 'red',ha = 'center',
-                        bbox = {'facecolor':'white','alpha':0.7,'boxstyle':'Round, pad=0.1'})
+#                 ax.text(x_text[i_text],y_text,text[i_text],color = 'red',ha = 'center',
+#                         bbox = {'facecolor':'white','alpha':0.7,'boxstyle':'Round, pad=0.1'})
             
-            i_sp +=1
+#             i_sp +=1
             
-            fig_num +=1
+#             fig_num +=1
             
-            if col == fig_dim - 1:
+#             if col == fig_dim - 1:
                 
-                col = 0
-                row +=1
+#                 col = 0
+#                 row +=1
             
-            else:
+#             else:
                 
-                col += 1
+#                 col += 1
                 
 #%% compare results between species
 
