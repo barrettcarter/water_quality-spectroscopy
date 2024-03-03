7065e6f078328a36767ae97f1f22c9e7ad2dcb86
Carter <barrettcarter1991@gmail.com>
2024-03-03 18:00:44 -0500

    Update perf_met_summary_sample_treatments.py
    Modified for making 1:1 plots

--- Code/perf_met_summary_sample_treatments.py
+++ Code/perf_met_summary_sample_treatments.py
@@ -239,7 +239,7 @@ true_ests['Estimated Concentration'] = y_hat_test['value']
 ID_col = 'sample type, chemical analyte, ML algorithm, treatment'
 
 true_ests.sort_values(by = ID_col,inplace=True)
-#%% make 1:1 plot
+#%% make 1:1 plots for best performing model/treatment for each chemical analyte/sample type
 
 g = sns.lmplot(data = true_ests, x = 'True Concentration', y = 'Estimated Concentration',
            col = ID_col,col_wrap = 5,height = 3,
@@ -250,46 +250,144 @@ g.set_titles('{col_name}')
 
 plt.savefig(os.path.join(fig_dir,'Exp2_opt_11_plots.png'),dpi = 300)
 
-#%% make subset for 2 best fits for each sample type
+#%% make subset for 2 most significant/relevant effects of sample treatment
 
-stype = 'Streams'
+subs = ['Stream_TN','Stream_PO4-P','HNS_Fe','HNS_Zn']
 
-for stype in sample_types:
-    
-    stype_pm_opt = pm_summary.loc[(pm_summary.sample_type==stype)&\
-                                  (pm_summary.perf_met=='test_rsq'),:].sort_values(by = 'value',
-                                                                                  ascending=False)
-        
-    stype_pm_opt = stype_pm_opt.iloc[[0,1],:]
-    
-    if stype == sample_types[0]:
-        
-        pm_opt = stype_pm_opt
-        
-    else:
-        
-        pm_opt = pd.concat([pm_opt,stype_pm_opt],ignore_index=False)
-    
+true_ests_best = true_ests[(true_ests.sample_type+'_'+true_ests.species).isin(subs)]
 
-pm_opt_ids = pm_opt.set_index(['sample_type','species','model']).index
+true_ests_best.rename(columns = {'True Concentration':'True Concentration (mg/L)',
+                                 'Estimated Concentration':'Estimated Concentration (mg/L)'},inplace = True)
 
 #%% make 1:1 plots for best fits
 
 plt.figure(dpi = 300)
 
-true_ests_best = true_ests[true_ests.set_index(['sample_type','species','model']).index.isin(pm_opt_ids)]
+
+g = sns.lmplot(data = true_ests_best, x = 'True Concentration (mg/L)', y = 'Estimated Concentration (mg/L)',
+           col = ID_col,col_wrap = 2,height = 4,
+           facet_kws = {'sharey':False,'sharex':False},
+           scatter_kws = {'color':'grey','alpha': 0.5,'s':2})
+
+g.set_titles('{col_name}')
+
+plt.savefig(os.path.join(fig_dir,'Exp2_opt_best_11_plots.png'),dpi = 300)
+
+#%% compile the true and predicted values for ALL models
+### for making 1:1 plots showing all sample treatments
+
+pm_summary = pd.read_csv(os.path.join(path_to_wqs,'Outputs','perf_met_opt_summary_sample_treatments.csv'))
+
+pm_summary = pm_summary.loc[pm_summary.perf_met == 'test_rsq',:] # optimal models based on r-sq
+
+# opt_ests = pd.DataFrame(columns = ['sample_type','species','model','y_true','y_pred'])
+
+# proj_dir = path_to_wqs
+
+stype = 'Streams' # for testing.
+stype = sample_types[-1]
+
+aliases = exp_names
+
+for stype in sample_types:
+    
+    stype_al = aliases[stype]
+    
+    compiled_outputs = pd.read_csv(os.path.join(path_to_wqs,stype,'outputs',
+                                                f'{stype_al}_ML_results_compiled.csv'))
+    
+    compiled_outputs.sample_type = stype
+    
+    stype_pm_opt = pm_summary.loc[pm_summary.sample_type==stype,:]
+    
+    valid_comb = stype_pm_opt.set_index(['sample_type','species','model']).index
+
+    compiled_outputs = compiled_outputs[compiled_outputs.set_index(['sample_type','species','model']).index.isin(valid_comb)]
+    
+    compiled_outputs = compiled_outputs[compiled_outputs.output.isin(['y_true_test','y_hat_test'])]
+    
+    if stype == sample_types[0]:
+    
+        true_tests = compiled_outputs
+        
+    else:
+        
+        true_tests = pd.concat([true_tests,compiled_outputs],ignore_index=True)
+        
+#%% reshape dataframe
+
+st_map = {'Hydroponics':'HNS','Streams':'Stream'}
+
+sp_map = {'Nitrate-N':'NO3-N','Potassium':'K','Calcium':'Ca','Sulfate':'SO4',
+          'Phosphorus':'P','Magnesium':'Mg','Ammonium-N':'NH4-N',
+          'pH':'pH','Iron':'Fe','Manganese':'Mn','Boron':'B',
+          'Zinc':'Zn','Copper':'Cu','Molybdenum':'Mb',
+          'TKN':'TKN','ON':'ON','TN':'TN',
+          'Phosphate-P':'PO4-P','TP':'TP','OP':'OP'}
+
+trmt_map = {'comb':'combined','fil':'filtered','unf':'unfiltered','diluted':'diluted',
+            'undiluted':'undiluted'}
+
+true_tests['sample_type'] = true_tests['sample_type'].apply(lambda x: st_map[x])
+
+true_tests['species'] = true_tests['species'].apply(lambda x: sp_map[x])
+
+true_tests['treatment'] = true_tests['treatment'].apply(lambda x: trmt_map[x])
+
+# true_tests['sample type, chemical analyte, ML algorithm, treatment'] = true_tests['sample_type']+\
+#     ', '+true_tests['species']+', '+true_tests['model']+', '+true_tests['treatment']
+
+true_tests['sample type, chemical analyte, model'] = true_tests['sample_type']+\
+    ', '+true_tests['species']+', '+true_tests['model']
+
+y_true_test = true_tests[true_tests.output=='y_true_test'].reset_index(drop=True)
+
+y_hat_test = true_tests[true_tests.output=='y_hat_test'].reset_index(drop=True)
+
+true_ests = y_true_test.copy()
+
+true_ests.rename(columns = {'value':'True Concentration'},inplace=True)
+
+true_ests['Estimated Concentration'] = y_hat_test['value']
+
+ID_col = 'sample type, chemical analyte, model'
+
+true_ests.sort_values(by = ID_col,inplace=True)
+
+true_ests.rename(columns = {'treatment':'Sample Treatment'},inplace=True)
+#%% make 1:1 plots for best performing model/treatment for each chemical analyte/sample type
+
+g = sns.lmplot(data = true_ests, x = 'True Concentration', y = 'Estimated Concentration',
+           col = ID_col,col_wrap = 5,height = 3,scatter = False,hue = 'Sample Treatment',
+           facet_kws = {'sharey':False,'sharex':False},
+           scatter_kws = {'color':'grey','alpha': 0.5,'s':2})
+
+g.set_titles('{col_name}')
+
+plt.savefig(os.path.join(fig_dir,'Exp2_ALL_11_plots.png'),dpi = 300)
+
+#%% make subset for 2 most significant/relevant effects of sample treatment
+
+subs = ['Stream_TN','Stream_PO4-P','HNS_Fe','HNS_Zn']
+
+true_ests_best = true_ests[(true_ests.sample_type+'_'+true_ests.species).isin(subs)]
 
 true_ests_best.rename(columns = {'True Concentration':'True Concentration (mg/L)',
                                  'Estimated Concentration':'Estimated Concentration (mg/L)'},inplace = True)
 
+#%% make 1:1 plots for best fits
+
+plt.figure(dpi = 300)
+
+
 g = sns.lmplot(data = true_ests_best, x = 'True Concentration (mg/L)', y = 'Estimated Concentration (mg/L)',
-           col = ID_col,col_wrap = 2,height = 5,
+           col = ID_col,col_wrap = 2,height = 4,
            facet_kws = {'sharey':False,'sharex':False},
-           scatter_kws = {'color':'grey','alpha': 0.5})
+           scatter_kws = {'color':'grey','alpha': 0.5,'s':2})
 
 g.set_titles('{col_name}')
 
-plt.savefig(os.path.join(fig_dir,'Exp1_opt_best_11_plots.png'),dpi = 300)
+plt.savefig(os.path.join(fig_dir,'Exp2_opt_best_11_plots.png'),dpi = 300)
 
 #%% Scratch
 
