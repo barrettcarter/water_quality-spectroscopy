0f81599cb242a7b6cd10298e8a6459f2c65a0e14
Carter <barrettcarter1991@gmail.com>
2024-03-16 17:34:28 -0400

    Update filtration experiment code
    abs_summary_plots_sample_treatments.py and WQ_suammary_filtration.py. Added code to make more plots and generate some more stats tables.

--- Code/WQ_summary_filtration.py
+++ Code/WQ_summary_filtration.py
@@ -108,3 +108,140 @@ g = sns.catplot(data=wq_long,x='Chemical Analyte',y = 'Concentration (mg/L)',
 g.set_titles(col_template="")
 
 plt.savefig(os.path.join(fig_dir,'stream_wq_filtration_boxplot.png'),dpi = 300)
+
+#%% make useful variables for heatmaps
+
+wq_strm.rename(columns = labels_dict,inplace=True)
+
+wq_strm.loc[wq_strm.Filtered,'Filtration'] = 'Filtered'
+wq_strm.loc[wq_strm.Filtered==False,'Filtration'] = 'Unfiltered'
+
+filtration = wq_strm.Filtration.unique()
+
+#%% make heatmaps
+
+filt = 'Unfiltered' # for testing
+
+wq_corrs_dict = {}
+
+for filt in filtration:
+    
+    wq_df = wq_strm.loc[wq_strm.Filtration==filt,'NH4-N':'OP']
+    wq = wq_df[wq_df.isna().any(axis=1)==False]
+    
+    wq_ar = wq.to_numpy()
+    wq_corrs = np.corrcoef(wq_ar,rowvar = False)
+    
+    wq_corrs_dict[filt] = wq_corrs
+    
+    wq_map = np.empty(wq_corrs.shape)
+    
+    for r in range(wq_map.shape[0]):
+        for c in range(wq_map.shape[1]):
+            if r>c:
+                wq_map[r,c]=False
+            else:
+                wq_map[r,c]=True
+     
+    plt.figure(dpi = 300)
+    
+    plt.title(filt)
+    
+    sns.heatmap(wq_corrs,mask = wq_map,xticklabels = wq.columns,yticklabels = wq.columns,
+                vmin = -1, vmax = 1,center =0, cmap = 'coolwarm',
+                annot = True,annot_kws={'size':'x-small'},fmt = '.2f')
+    
+    # plt.savefig(os.path.join(fig_dir,f'{filt}_WQ_heatmap.png'),dpi=300,bbox_inches='tight')
+    
+#%% make difference heatmap
+
+wq_corrs_filt = wq_corrs_dict['Filtered']
+wq_corrs_unf = wq_corrs_dict['Unfiltered']
+
+wq_corrs_dif = wq_corrs_filt - wq_corrs_unf
+
+mean_corr_dif = np.mean(wq_corrs_dif)
+
+# wq_corrs_pdif = np.round(wq_corrs_dif/wq_corrs_unf*100).astype(int)
+
+wq_corrs_pdif = wq_corrs_dif/np.abs(wq_corrs_unf)
+
+diff_dict = {'Difference':wq_corrs_dif,'Percent Difference': wq_corrs_pdif}
+
+fmt_dict = {'Difference':'.2f','Percent Difference':'.0%'}
+
+for diff in list(diff_dict.keys()):
+    
+    wq_corrs = diff_dict[diff]
+    
+    plt.figure(dpi = 300)
+    
+    plt.title(diff)
+    
+    sns.heatmap(wq_corrs,mask = wq_map,xticklabels = wq.columns,yticklabels = wq.columns,
+                center =0, cmap = 'coolwarm',
+                annot = True,annot_kws={'size':'x-small'},fmt = fmt_dict[diff])
+    
+    plt.savefig(os.path.join(fig_dir,f'{diff}_WQ_heatmap.png'),dpi=300,bbox_inches='tight')
+    
+#%% r-squared heatmaps
+
+wq_rsq_filt = wq_corrs_filt**2
+wq_rsq_unf = wq_corrs_unf**2
+
+wq_rsq_dict = {'Filtered':wq_rsq_filt,'Unfiltered':wq_rsq_unf}
+
+for filtration in ['Filtered','Unfiltered']:
+    
+    wq_corrs = wq_rsq_dict[filtration]
+    
+    wq_map = np.empty(wq_corrs.shape)
+    
+    for r in range(wq_map.shape[0]):
+        for c in range(wq_map.shape[1]):
+            if r>c:
+                wq_map[r,c]=False
+            else:
+                wq_map[r,c]=True
+     
+    plt.figure(dpi = 300)
+    
+    plt.title(f'$r^2$ {filtration}')
+    
+    sns.heatmap(wq_corrs,mask = wq_map,xticklabels = wq.columns,yticklabels = wq.columns,
+                vmin = 0, vmax = 1,center =0.5, cmap = 'Reds',
+                annot = True,annot_kws={'size':'x-small'},fmt = '.3f')
+    
+    plt.savefig(os.path.join(fig_dir,f'{filtration}_WQ_rsq_heatmap.png'),dpi=300,bbox_inches='tight')
+    
+#%% make difference heatmap
+
+wq_rsq_dif = wq_rsq_filt - wq_rsq_unf
+
+mean_rsq_fil = np.mean(wq_rsq_filt)
+
+mean_rsq_unf = np.mean(wq_rsq_unf)
+
+mean_rsq_dif = np.mean(wq_rsq_dif)
+
+# wq_rsq_pdif = np.round(wq_rsq_dif/wq_rsq_unf*100).astype(int)
+
+wq_rsq_pdif = wq_rsq_dif/np.abs(wq_rsq_unf)
+
+diff_dict = {'Difference':wq_rsq_dif,'Percent Difference': wq_rsq_pdif}
+
+fmt_dict = {'Difference':'.2f','Percent Difference':'.0%'}
+
+for diff in list(diff_dict.keys()):
+    
+    wq_rsq = diff_dict[diff]
+    
+    plt.figure(dpi = 300)
+    
+    plt.title(diff)
+    
+    sns.heatmap(wq_rsq,mask = wq_map,xticklabels = wq.columns,yticklabels = wq.columns,
+                center =0, cmap = 'coolwarm',
+                annot = True,annot_kws={'size':'x-small'},fmt = fmt_dict[diff])
+    
+    plt.savefig(os.path.join(fig_dir,f'{diff}_WQ_rsq_heatmap.png'),dpi=300,bbox_inches='tight')
--- Code/abs_summary_plots_sample_treatments.py
+++ Code/abs_summary_plots_sample_treatments.py
@@ -50,6 +50,8 @@ fig_dir = f'C:\\Users\\{user}\\OneDrive\\Research\\PhD\\Communications\\Images'
 hns_inter_dir = os.path.join(path_to_wqs,'Hydroponics','intermediates')
 
 streams_inter_dir = os.path.join(path_to_wqs,'Streams','intermediates')
+
+output_dir = os.path.join(path_to_wqs,'Outputs')
 #%% Bring in data and concatenate
 
 abs_df = pd.read_csv(os.path.join(hns_inter_dir,'abs-wq_HNSr_df.csv'))
@@ -81,6 +83,10 @@ for file,label in zip(files,labels):
     abs_df = pd.concat([abs_df,abs_sub])
 
 abs_med = abs_df.groupby('sample_type').median().T
+
+abs_av = abs_df.groupby('sample_type').mean().T
+
+abs_sd = abs_df.groupby('sample_type').std().T
 
 abs_q1 = abs_df.groupby('sample_type').quantile(0.25).T
 
@@ -115,3 +121,81 @@ for stype in sample_types:
     ax.set_title(stype)
     
     iax +=1
+    
+#%% make difference plot
+
+abs_av_diffs = pd.DataFrame(columns = ['HNS','Streams'])
+
+abs_av_diffs['HNS'] = abs_av['HNS - Diluted'] - abs_av['HNS - Undiluted']
+
+abs_av_diffs['Streams'] = abs_av['Stream - Filtered'] - abs_av['Stream - Unfiltered']
+
+abs_sd_diffs = pd.DataFrame(columns = ['HNS','Streams'])
+
+abs_sd_diffs['HNS'] = abs_sd['HNS - Diluted'] - abs_sd['HNS - Undiluted']
+
+abs_sd_diffs['Streams'] = abs_sd['Stream - Filtered'] - abs_sd['Stream - Unfiltered']
+
+fig, axs = plt.subplots(nrows = 1, ncols = 2, sharey = False, dpi = 300,
+                        figsize = [6.5,3.5], tight_layout = True)
+
+fig.text(0.5,0,'wavelength (nm)',ha = 'center',va = 'top')
+
+axs[0].plot(wavelengths,abs_av_diffs['HNS'], label = 'average')
+axs[1].plot(wavelengths,abs_av_diffs['Streams'], label = 'average')
+
+axs[0].plot(wavelengths,abs_sd_diffs['HNS'], label = 'SD')
+axs[1].plot(wavelengths,abs_sd_diffs['Streams'], label = 'SD')
+
+axs[0].set_ylabel('change in absorbance (unitless)')
+axs[0].set_title('HNS diluted - undiluted')
+
+axs[1].set_title('Streams filtered - unfiltered')
+
+axs[1].legend()
+
+
+    
+#%% make some statistics
+
+abs_med_stats = abs_med.describe().T
+
+abs_av_stats = abs_av.describe().T.reset_index(names = ['Sample Type - Treatment'])
+
+abs_av_stats['Statistic'] = 'mean'
+
+abs_sd_stats = abs_sd.describe().T.reset_index(names = ['Sample Type - Treatment'])
+
+abs_sd_stats['Statistic'] = 'SD'
+
+abs_stats = pd.concat([abs_av_stats,abs_sd_stats],ignore_index = True)
+
+abs_stats['Sample Type'] = abs_stats['Sample Type - Treatment'].apply(lambda x: x.split(' - ')[0])
+
+abs_stats['Treatment'] = abs_stats['Sample Type - Treatment'].apply(lambda x: x.split(' - ')[1])
+
+abs_stats.drop(columns = ['Sample Type - Treatment'],inplace = True)
+
+abs_stats.set_index(['Sample Type', 'Treatment', 'Statistic'],inplace = True)
+
+abs_stats = abs_stats.T
+
+#%% save statistics
+
+abs_stats.to_csv(os.path.join(output_dir,'abs_stats_sample_treatments.csv'))
+
+#%% scratch
+
+HNS_max_diff = abs_av_diffs[abs_av_diffs.HNS == abs_av_diffs.HNS.min()]['HNS']
+
+HNS_max_diff_band = int(list(HNS_max_diff.index)[0].split('_')[1])
+
+HNS_max_diff_wl = wavelengths[HNS_max_diff_band-1]
+
+Strm_max_diff = abs_av_diffs[abs_av_diffs.Streams == abs_av_diffs.Streams.min()]['Streams']
+
+Strm_max_diff_band = int(list(Strm_max_diff.index)[0].split('_')[1])
+
+Strm_max_diff_wl = wavelengths[Strm_max_diff_band-1]
+
+
